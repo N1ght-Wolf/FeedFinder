275e55685a1d53a6312cc1834ca8567675fbafdd
David Oyeku <d.oyeku@ncl.ac.uk>
2016-04-18 21:06:27 +0100

    updated the user model
    updated the user model so it uses the users table config

--- app/Model/User.php
+++ app/Model/User.php
@@ -10,34 +10,38 @@ App::uses('Review', 'Model');
  *
  * @package       app.Model
  */
-class User extends Model {
+class User extends Model
+{
+    public $useDbConfig = 'users';
+    var $name = 'User';
+    var $hasMany = 'Review';
 
-  var $name = 'User';
-  var $hasMany = 'Review';
 
-  public function route($query){
-    return $this->calculateInterquartile($query);
-  }
+    public function route($query)
+    {
+        return $this->calculateInterquartile($query);
+    }
 
- public function calculateInterquartile($query){
+    public function calculateInterquartile($query)
+    {
         $Model = $AnotherModel = ClassRegistry::init($query['explore']['pg_table']);
         $quartile = array();
-        $category =strtolower($query['category']['name']);
+        $category = strtolower($query['category']['name']);
         $attr_name = $query['time']['attr_name'];
         //e.g. venue_all
-        $column = $category.$attr_name;
+        $column = $category . $attr_name;
         $results = $Model->query("select $column, ntile(5) over (order by $column) as quartile from counties group by $column order by $column desc");
-        
+
         foreach ($results as $result => $value) {
-             $q = $value['0']['quartile'];
-            if (!array_key_exists($q, $quartile)){
+            $q = $value['0']['quartile'];
+            if (!array_key_exists($q, $quartile)) {
                 $quartile[$q] = $value['0'][$column];
             }
         }
 
-        $style='feedfinder_'.strtolower($query['category']['name']).$query['time']['attr_name'].'_sld';
+        $style = 'feedfinder_' . strtolower($query['category']['name']) . $query['time']['attr_name'] . '_sld';
 
-        return array('quartiles'=>$quartile,'style'=>$style,'layer'=>$Model->table);
+        return array('quartiles' => $quartile, 'style' => $style, 'layer' => $Model->table);
     }
 
 
