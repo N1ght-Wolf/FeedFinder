a6654675bc22853115f24fde34b36df52a9de7d1
David Oyeku <d.oyeku@ncl.ac.uk>
2016-04-20 13:20:52 +0100

    sfa

--- app/Controller/DashboardsController.php
+++ app/Controller/DashboardsController.php
@@ -47,9 +47,9 @@ class DashboardsController extends AppController
             $result = $this->$model->route($query);
 
             //$json = array('result'=>$result);
-            echo "<pre>";
-            print_r($result);
-            echo "</pre>";
+//            echo "<pre>";
+//            print_r($result);
+//            echo "</pre>";
 //            header('Content-type: application/json');
             echo json_encode($result);
             exit;
