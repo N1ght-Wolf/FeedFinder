6a50463d3208355fc0eb9ba7ae0609e69dc87671
David Oyeku <d.oyeku@ncl.ac.uk>
2016-04-20 11:04:38 +0100

    moved the county shell outside

---
+++ app/Console/updateCountyShell.php
@@ -0,0 +1,19 @@
+<?php 
+class updateCountyShell extends AppShell {
+	public $uses = array('County','Soa');
+
+	public function updateCounty(){
+		$result = $this->County->update();
+	}
+
+	public function updateSuperOutArea(){
+		$this->Soa->update();
+	}
+
+	public function updateVenueCounty(){
+		$this->County->updateCountyId();
+	}
+}
+
+?>
+//cd /Applications/MAMP/htdocs/FeedFinder/app && Console/cake updateCounty updateVenueCounty
