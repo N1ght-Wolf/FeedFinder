014447e9ea4c055525e07692fb487613982e5b42
David Oyeku <d.oyeku@ncl.ac.uk>
2016-04-20 11:24:44 +0100

    made new file hello world.php

---
+++ app/Console/Command/HelloShell.php
@@ -0,0 +1,13 @@
+<?php
+class HelloShell extends AppShell {
+    public function main() {
+        $this->out('Hello world.');
+    }
+}
+?>
+/**
+ * Created by PhpStorm.
+ * User: DavidOyeku
+ * Date: 20/04/2016
+ * Time: 11:24
+ */
